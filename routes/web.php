e2788582d1bab39285e43843b920f5c4c1022886
IshqhsI <oneridh18@gmail.com>
2025-05-30 12:08:44 +0700

    update route for dashboard: action with controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
@@ -16,9 +17,7 @@ Route::middleware([
     'verified',
 ])->group(function () {
 
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
     Route::name('admin.')->prefix('admin')->group(function () {
         Route::resource('/categories', CategoryController::class);
